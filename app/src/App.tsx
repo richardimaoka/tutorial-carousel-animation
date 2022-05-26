fdf087699042ecce1379ef16a466087e4f94a620
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-05-26 23:32:42 +0900

    overflow-x: hidden

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,30 +2,42 @@ import React from "react";
 
 function App() {
   return (
-    <div style={{ display: "flex", flexDirection: "row" }}>
-      <div style={{ padding: "4px" }}>
-        <img
-          width="640px"
-          height="360px"
-          style={{ verticalAlign: "middle" }}
-          src="/images/1.png"
-        />
-      </div>
-      <div style={{ padding: "4px" }}>
-        <img
-          width="640px"
-          height="360px"
-          style={{ verticalAlign: "middle" }}
-          src="/images/2.png"
-        />
-      </div>
-      <div style={{ padding: "4px" }}>
-        <img
-          width="640px"
-          height="360px"
-          style={{ verticalAlign: "middle" }}
-          src="/images/3.png"
-        />
+    <div
+      style={{
+        width: "648px",
+        overflowX: "auto",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <div style={{ padding: "4px" }}>
+          <img
+            width="640px"
+            height="360px"
+            style={{ verticalAlign: "middle" }}
+            src="/images/1.png"
+          />
+        </div>
+        <div style={{ padding: "4px" }}>
+          <img
+            width="640px"
+            height="360px"
+            style={{ verticalAlign: "middle" }}
+            src="/images/2.png"
+          />
+        </div>
+        <div style={{ padding: "4px" }}>
+          <img
+            width="640px"
+            height="360px"
+            style={{ verticalAlign: "middle" }}
+            src="/images/3.png"
+          />
+        </div>
       </div>
     </div>
   );
