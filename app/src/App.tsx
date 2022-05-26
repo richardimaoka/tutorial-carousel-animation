b9e208a4c875bc328664507ac256a1867c274a3a
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-05-26 23:57:44 +0900

    add barebone buttons

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -48,6 +48,9 @@ const CarouselControl = () => {
   return (
     <div>
       <CarouselContainer imagePathList={imagePathList} />
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
     </div>
   );
 };
