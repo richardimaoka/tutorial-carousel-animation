c9e120e421e2200165727b3aeee0f2b5a5238243
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-05-29 14:45:30 +0900

    add the missing patch

--- scripts/git-apply-print.sh
+++ scripts/git-apply-print.sh
@@ -3,4 +3,4 @@
 cd "$(dirname "$0")" || exit 
 cd ../ || exit # cd to the git repository root
 
-git log --oneline --reverse 84ffe06..dc61691 | sed -r 's/(^\w+\b)/git apply patches\/\1.patch #/'
+git log --oneline --reverse dc61691 | sed -r 's/(^\w+\b)/git apply patches\/\1.patch #/'
--- scripts/git-patch-gen.sh
+++ scripts/git-patch-gen.sh
@@ -3,6 +3,6 @@
 cd "$(dirname "$0")" || exit 
 cd ../ || exit # cd to the git repository root
 
-git log --oneline --reverse 84ffe06..dc61691 | \
+git log --oneline --reverse dc61691 | \
    sed -r 's/(^\w+\b)(.*)/git show --binary --src-prefix=a\/client\/ --dst-prefix=b\/client\/ --patch \1 > patches\/\1.patch/'
    
